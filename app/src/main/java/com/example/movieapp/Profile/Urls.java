5a6145980a352e51cdb48d685b31bea87680c56a
lone20 <long2011200@gmail.com>
2021-10-18 16:30:21 +0700

    nộp lại lần nữa

---
+++ app/src/main/java/com/example/movieapp/Profile/Urls.java
@@ -0,0 +1,8 @@
+package com.example.movieapp.Profile;
+
+public class Urls {
+    public static final String ROOT_URL = "http:/192.168.1.5/mysql/";
+    public static final String RESET_PASSWORD_URL = ROOT_URL + "resetpassword.php";
+    public static final String UPDATE_USER_URL = ROOT_URL + "update.php";
+
+}
