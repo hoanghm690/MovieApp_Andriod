2f3005065dfcf2f0343c07cea1b84a58fe34dca8
lone20 <long2011200@gmail.com>
2021-10-18 17:54:54 +0700

    Nộp Profile, login, register

---
+++ app/src/main/java/com/example/movieapp/Urls/Urls.java
@@ -0,0 +1,10 @@
+package com.example.movieapp.Urls;
+
+public class Urls {
+    public static final String ROOT_URL = "http:/192.168.1.11/mysql/";//Đổi theo localhost của máy
+    public static final String REGISTER_URL = ROOT_URL+"register.php";
+    public static final String LOGIN_URL = ROOT_URL+"login.php";
+    public static final String RESET_PASSWORD_URL = ROOT_URL + "resetpassword.php";
+    public static final String UPDATE_USER_URL = ROOT_URL + "update.php";
+
+}
