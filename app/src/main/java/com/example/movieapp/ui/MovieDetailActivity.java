68f244fe919209d4f1f13650a5073a5c35e074cd
TonTon69 <hoanghuynh0609@gmail.com>
2021-10-25 15:45:53 +0700

    call api movies detail & movies related

--- app/src/main/java/com/example/movieapp/ui/MovieDetailActivity.java
+++ app/src/main/java/com/example/movieapp/ui/MovieDetailActivity.java
@@ -3,7 +3,9 @@ package com.example.movieapp.ui;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.movieapp.R;
+import com.example.movieapp.Urls.Urls;
 import com.example.movieapp.adapters.MovieAdapter;
+import com.example.movieapp.adapters.MovieItemClickListener;
 import com.example.movieapp.adapters.ViewPagerAdapter;
 import com.example.movieapp.api.ApiUtils;
 import com.example.movieapp.api.MovieService;
@@ -11,9 +13,11 @@ import com.example.movieapp.models.Phim;
 import com.example.movieapp.models.ResponseParser;
 import com.squareup.picasso.Picasso;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,7 +35,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MovieDetailActivity extends AppCompatActivity {
+public class MovieDetailActivity extends AppCompatActivity implements MovieItemClickListener {
 
     private ImageView MovieImg, MovieCoverImg;
     private TextView MovieTitle, MovieCategory;
@@ -39,7 +43,7 @@ public class MovieDetailActivity extends AppCompatActivity {
     private MovieAdapter movieAdapter;
     private MovieService movieService;
     private Call<ResponseParser> call;
-    private List<Phim> listMovies;
+    private List<Phim> listMovies, movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,28 +79,47 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
     void iniMovieRelated() {
-//        call = movieService.getListMovies("IwAR1k4WlQbyCdrKT7ITP-6RrfGhyIk-IFtByEE2uM_vBn_PWgXASG0mnaXF0");
-//        call.enqueue(new Callback<ResponseParser>() {
-//            @Override
-//            public void onResponse(Call<ResponseParser> call, Response<ResponseParser> response) {
-//                ResponseParser responseParser = response.body();
-//
-//                if (responseParser != null) {
-//                    listMovies = new ArrayList<>();
-//                    for (int i = 5; i < 10; i++) {
-//                        listMovies.add(responseParser.getPhim().get("phimle").get(i));
-//                    }
-//                    movieAdapter = new MovieAdapter(MovieDetailActivity.this, listMovies);
-//                    RvMoiveRelated.setAdapter(movieAdapter);
-//                    RvMoiveRelated.setLayoutManager(new GridLayoutManager(MovieDetailActivity.this, 3));
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseParser> call, Throwable t) {
-//                t.printStackTrace();
-//                Toast.makeText(MovieDetailActivity.this, "Call api error", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        call = movieService.getListMovies(Urls.API_PARAMS);
+        call.enqueue(new Callback<ResponseParser>() {
+            @Override
+            public void onResponse(Call<ResponseParser> call, Response<ResponseParser> response) {
+                ResponseParser responseParser = response.body();
+
+                if (responseParser != null) {
+                    listMovies = new ArrayList<>();
+                    movies = new ArrayList<>();
+                    movies = responseParser.getPhim().get("phimle");
+                    String movieCate = getIntent().getExtras().getString("category");
+
+                    for (int i = 0; i < movies.size(); i++) {
+                        if (movies.get(i).getCategory().contains(movieCate)) {
+                            listMovies.add(movies.get(i));
+                        }
+                    }
+                    Log.v("list",""+listMovies);
+                    movieAdapter = new MovieAdapter(MovieDetailActivity.this, listMovies,MovieDetailActivity.this);
+                    RvMoiveRelated.setAdapter(movieAdapter);
+                    RvMoiveRelated.setLayoutManager(new LinearLayoutManager(MovieDetailActivity.this, LinearLayoutManager.HORIZONTAL, false));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseParser> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(MovieDetailActivity.this, "Call api error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onMovieClick(Phim movie, ImageView movieImageView) {
+        Intent intent = new Intent(this, MovieDetailActivity.class);
+        intent.putExtra("title", movie.getTitle());
+        intent.putExtra("imgURL", movie.getImageUrl());
+        intent.putExtra("category", movie.getCategory());
+        intent.putExtra("url", movie.getUrl());
+        startActivity(intent);
+
+        Toast.makeText(this, "item clicked: " + movie.getTitle(), Toast.LENGTH_SHORT).show();
     }
 }
